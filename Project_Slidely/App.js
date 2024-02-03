8db372d6b669e32f9a127eec8c3afb9141f23147
Reema Alzahrani <114014846+reabzh@users.noreply.github.com>
2024-02-03 17:54:04 +0300

    Update App.js

--- Project_Slidely/App.js
+++ Project_Slidely/App.js
@@ -1,16 +1,70 @@
-import { View, Text , StyleSheet } from 'react-native'
-import React from 'react'
-import Home from './src/Screens/Home'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Welcome from './src/Screens/WelcomePage';
+import Home from './src/Screens/Home';
+import Login from './src/Screens/Login';
+import Signup from './src/Screens/SignUp';
+import Profile from './src/Screens/Profile';
+import Categories from './src/Screens/Categories';
+// import Search from './src/Screens/Search'
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tab_in_Stack(){
+  return(
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Profile') {
+          iconName = focused ? 'person' : 'person-outline';
+        } else if (route.name === 'Categories') {
+          iconName = focused ? 'list' : 'list-outline';
+        } else if (route.name === 'Search') {
+          iconName = focused ? 'android-search' : 'android-search-outline';
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#009999',
+      tabBarInactiveTintColor: 'gray',
+      headerShown: false,
+    })}>
+      <Tab.Screen name='Home' component={Home}></Tab.Screen>
+      <Tab.Screen name='Categories' component={Categories}></Tab.Screen>
+      {/* <Tab.Screen name='Search' component={Search}></Tab.Screen> */}
+      <Tab.Screen name='Profile' component={Profile}></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+function Login_and_Signup(){
+  return(
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name='Login' component={Login}></Stack.Screen>
+    <Stack.Screen name='Signup' component={Signup}></Stack.Screen>
+  </Stack.Navigator>
+  );
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.Container}>
-      <Home />
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name='Welcome' component={Welcome}></Stack.Screen>
+        <Stack.Screen name='PageOne' component={Login_and_Signup}></Stack.Screen>
+        <Stack.Screen name='PageTwo' component={Tab_in_Stack}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-const styles = StyleSheet.create({
-  Container:{
-    flex:1
-  }
-});
+
+
