d06a01a2af14b968ea4f7a75408cd14e75fa1978
Reema Alzahrani <114014846+reabzh@users.noreply.github.com>
2024-02-03 18:18:03 +0300

    Add files via upload

---
+++ Project_Slidely/src/Screens/SignUp.js
@@ -0,0 +1,129 @@
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+export default function SignUp({ props }) {
+  const navigation = useNavigation(); // Initialize the navigation hook
+
+  return (
+    <View>
+      <View style={styles.loginContainer}>
+        <Image style={styles.logo} source={require('./Images/Bookstore_logo.png')}></Image>
+      </View>
+
+      <View style={styles.loginContainer}>
+        <Text style={styles.text_header}> Sign Up</Text>
+        <View style={styles.action}>
+          <FontAwesome name='user-o' color='#009999' style={styles.smallIcon}></FontAwesome>
+          <TextInput placeholder='Name' style={styles.textInput} ></TextInput>
+        </View>
+
+
+        <View style={styles.action}>
+          <FontAwesome name='envelope' color='#009999' style={styles.smallIcon}></FontAwesome>
+          <TextInput placeholder='Email' style={styles.textInput} ></TextInput>
+        </View>
+
+        <View style={styles.action}>
+          <FontAwesome name='mobile' color='#009999' style={styles.smallIcon}></FontAwesome>
+          <TextInput placeholder='Email' style={styles.textInput} ></TextInput>
+        </View>
+
+
+        <View style={styles.action}>
+          <FontAwesome name='lock' color='#009999' style={styles.smallIcon}></FontAwesome>
+          <TextInput placeholder='Create Password' style={styles.textInput} ></TextInput>
+        </View>
+
+        <View style={styles.action}>
+          <FontAwesome name='lock' color='#009999' style={styles.smallIcon}></FontAwesome>
+          <TextInput placeholder='Confirm Password' style={styles.textInput} ></TextInput>
+        </View>
+
+
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.inBut} onPress={() => { navigation.navigate('PageTwo') }}>
+            <View>
+              <Text style={styles.textSign}> Sign Up</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+
+      </View>
+    </View>
+
+  )
+}
+
+
+
+
+const styles = StyleSheet.create({
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  smallIcon: {
+    marginRight: 10,
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  logo: {
+    height: 260,
+    width: 260,
+    marginTop: 30,
+    marginLeft: 50,
+  },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    paddingTop: 14,
+    paddingBottom: 3,
+    marginTop: 15,
+    paddingHorizontal: 15,
+    borderWidth: 1,
+    borderColor: '#009999',
+    borderRadius: 50,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: -12,
+    // color: '#009999',
+  },
+  loginContainer: {
+    // backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: '#009999',
+    // fontWeight: 'bold',
+    fontSize: 30,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: -20,
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: 20,
+  },
+  inBut: {
+    width: '70%',
+    backgroundColor: '#009999',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderRadius: 50,
+    marginTop: 50,
+  },
+})
---
+++ Project_Slidely/src/Screens/WelcomePage.js
@@ -0,0 +1,95 @@
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+export default function Login({ props }) {
+  const navigation = useNavigation(); // Initialize the navigation hook
+
+  return (
+    <View>
+      <View style={styles.loginContainer}>
+        <Image style={styles.logo} source={require('./Images/Bookstore_logo.png')}></Image>
+      </View>
+
+      <View style={styles.border}>
+        <Text style={styles.text_header}> Join Slidely Application!</Text>
+
+        <Text style={styles.txt}> Create an account to start reading</Text>
+
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.inBut} onPress={() => { navigation.navigate('PageOne') }}>
+            <View>
+              <Text style={styles.textSign}> Get Started </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </View>
+
+  )
+}
+
+
+
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    // backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  logo: {
+    height: 260,
+    width: 260,
+    marginTop: 30,
+    marginLeft: 50,
+  },
+  text_header: {
+    color: '#009999',
+    fontWeight: 'bold',
+    fontSize: 33,
+    marginTop: 70,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: -20,
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: 20,
+  },
+  inBut: {
+    width: '50%',
+    backgroundColor: '#009999',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderRadius: 50,
+    marginTop: 50,
+    marginLeft: 180,
+  },
+
+  border: {
+    borderColor: 'white',
+    borderWidth: 1, 
+    backgroundColor: 'white', 
+    borderRadius: 20, 
+    width: 410, 
+    height: 500, 
+  },
+  txt: {
+    fontSize: 20, 
+    color: 'grey', 
+    marginLeft: 15, 
+    padding: 5,
+  },
+})
