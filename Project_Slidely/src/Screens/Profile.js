d70cd6f2388e3e801a7c0a12dd795ed7f6360712
Arwa-155 <156934425+Arwa-155@users.noreply.github.com>
2024-02-05 21:14:07 +0300

    Update Profile.js

--- Project_Slidely/src/Screens/Profile.js
+++ Project_Slidely/src/Screens/Profile.js
@@ -1,195 +1,208 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, Linking, TextInput } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image , ScrollView , TextInput} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Title, Card, Button } from 'react-native-paper';
-import { MaterialIcons, Entypo, FontAwesome5 } from '@expo/vector-icons';
-import { ref, get } from "firebase/database";
-import { realtimeDb } from './firebaseConfig';
-import { useRoute } from '@react-navigation/native';
-import { uid } from "uid";
+import { MaterialIcons, Entypo, FontAwesome5, Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-const Profile = () => {
-  const route = useRoute();
-  const { id } = route.params;
-  const [user, setUser] = useState(null);
-  const [newName, setNewName] = useState('');
-  const [newEmail, setNewEmail] = useState('');
-  const [isEditing, setIsEditing] = useState(false);
+import { auth, db, realtimeDb } from '../../firebase'
+import { doc, getDoc , updateDoc} from 'firebase/firestore';
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const userDataRef = ref(realtimeDb, id);
-        const userDataSnapshot = await get(userDataRef);
-        if (userDataSnapshot.exists()) {
-          setUser(userDataSnapshot.val());
+
+
+const Profile = ({ props }) => {
+    const navigation = useNavigation(); // Initialize the navigation hook
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [phone, setPhone] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [ViewMode, setViewMode] = React.useState(true);
+
+    const handleSave = () => {
+        setViewMode(true)
+        updateUserDate();
+    }
+
+    const getUser = async () => {
+        const docRef = doc(db, 'users', auth.currentUser.uid);
+        const docsnap = await getDoc(docRef);
+        if (docsnap.exists()) {
+            console.log('Document data: ', docsnap.data());
+            const data = docsnap.data()
+            setName(data.name)
+            setEmail(data.email)
+            setPhone(data.phone)
+            setPassword(data.password) 
         } else {
-          console.log("User not found");
+            console.log('No such document');
         }
-      } catch (error) {
-        console.log(error);
-      }
     };
 
-    getUserData();
-  }, [id]);
+    const updateUserDate = async () => {
+        const washingToRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(washingToRef, {
+            name: name,
+            email: email,
+            phone: phone,
+            password: password,
+        })
+    };
 
-  if (!user) {
-    return <Text>Loading...</Text>;
-  }
-
-  const { name, email } = user;
-
-  const OpenDial = () => {
-    let phoneNumber = '';
-    if (Platform.OS === 'android') {
-      phoneNumber = 'tel:${01212836883}';
-    } else {
-      phoneNumber = 'telprompt:${01212836883}';
+    const handleEdit = () => {
+        setViewMode(false)
     }
 
-    Linking.openURL(phoneNumber);
-  };
+    { ViewMode ? getUser(): null }
+    return (
+        <ScrollView style={styles.root}>
+            <LinearGradient
+                colors={['#009999', '#00BFBF']}
+                style={{ height: "20%" }}
+            >
+                <View style={{ flexDirection: 'row', marginTop: "10%" }}>
+                    <View style={styles.divider} />
+                    <View style={styles.title}>
+                        <Text style={{ fontSize: 38, color: "#2D3436" }}> Personal <Text style={{ fontWeight: "300", color: "white" }}>Profile</Text></Text>
+                    </View>
+                    <View style={styles.divider} />
+                </View>
+            </LinearGradient>
 
-  const updateUser = async () => {
-    try {
-      if (newName && newEmail) {
-        const userDataRef = ref(realtimeDb, id);
-        await set(userDataRef, { name: newName, email: newEmail });
-
-        setUser({ ...user, name: newName, email: newEmail });
-
-        setIsEditing(false); // Hide input boxes after updating
-
-        console.log('User data updated successfully.');
-      } else {
-        console.log('Invalid input. Please provide both name and email.');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <View style={styles.root}>
-      <LinearGradient colors={['#009999', '#00BFBF']} style={{ height: '20%' }}>
-        <View style={{ flexDirection: 'row', marginTop: '10%' }}>
-          <View style={styles.divider} />
-          <View style={styles.title}>
-            <Text style={{ fontSize: 38, color: '#2D3436' }}>
-              Personal <Text style={{ fontWeight: '300', color: 'white' }}>Profile</Text>
-            </Text>
-          </View>
-          <View style={styles.divider} />
-        </View>
-      </LinearGradient>
-      <View style={styles.imagestyle}>
-        <Image style={{ width: 140, height: 140, borderRadius: 140 / 2, margin: -50 }} source={require('./Images/icon.png')} />
-      </View>
-
-
-      <Card style={styles.mycard} onPress={() => { Linking.openURL('mailto:Noor@gmail.com') }}>
-        <View style={styles.cardconent}>
-          <MaterialIcons style={{ margin: 4 }} name="email" size={32} color="#009999" />
-          {isEditing ? (
-            <TextInput
-              
-              placeholder="New Email"
-              value={newEmail}
-              onChangeText={setNewEmail}
-            />
-          ) : (
-            <Text style={{ marginTop: 12, fontSize: 15 }}>{email}</Text>
-          )}
-        </View>
-      </Card>
-
-      <Card style={styles.mycard} onPress={() => { OpenDial() }}>
-        <View style={styles.cardconent}>
-          <Entypo style={{ margin: 4 }} name="phone" size={32} color="#009999" />
-          <Text style={{ marginTop: 12, fontSize: 15 }}>01212836883</Text>
-        </View>
-      </Card>
-
-      <Card style={styles.javascriptmycard}>
-        <View style={styles.cardconent}>
-          <FontAwesome5 style={{ margin: 4 }} name="home" size={24} color="#009999" />
-          <Text style={{ marginTop: 12, fontSize: 15 }}>129 El-Merghany Street, Heliopolis</Text>
-        </View>
-      </Card>
-
-
-      <View style={{ alignItems: 'center', marginTop: 55, margin: 15 }}>
-        {isEditing ? (
-          <View>
-            <View style={styles.input}>
-            <TextInput
-              
-              placeholder="update name"
-              value={newName}
-              onChangeText={setNewName}
-            />
+            <View style={styles.imagestyle}>
+                <Image
+                    style={{ width: 140, height: 140, borderRadius: 140 / 2, margin: -50 }}
+                    source={require('./Images/icon.png')}
+                />
             </View>
-            <TextInput
-              style={styles.input}
-              placeholder="update Email"
-              value={newEmail}
-              onChangeText={setNewEmail}
-            />
-          </View>
-        ) : (
-          <Title>{name}</Title>
-        )}
-      </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 15 }}>
-        {isEditing ? (
-          <Button icon="account-check" color="#FF8C00" mode="contained" onPress={updateUser}>
-            Save
-          </Button>
-        ) : (
-          <Button icon="account-edit" color="#FF8C00" mode="contained" onPress={() => setIsEditing(true)}>
-            Edit
-          </Button>
-        )}
-      </View>
-    </View>
-  );
-};
 
+            <View style={{ alignItems: "center", marginTop: 55, margin: 15 }}>
+                <Title>{name}</Title>
+
+            </View>
+
+            { ViewMode ? (
+                <View>
+                    <Card style={styles.mycard}>
+                        <View style={styles.cardconent}>
+                            <Ionicons style={{ margin: 4 }} name="person" size={32} color='#009999' />
+                            <Text style={{ marginTop: 12, fontSize: 15 }}> {name} </Text>
+                        </View>
+                    </Card>
+
+                    <Card style={styles.mycard} >
+                        <View style={styles.cardconent}>
+                            <MaterialIcons style={{ margin: 4 }} name="email" size={32} color='#009999' />
+                            <Text style={{ marginTop: 12, fontSize: 15 }}> {email} </Text>
+
+                        </View>
+                    </Card>
+
+
+                    <Card style={styles.mycard} >
+                        <View style={styles.cardconent}>
+                            <Entypo style={{ margin: 4 }} name="phone" size={32} color='#009999' />
+                            <Text style={{ marginTop: 12, fontSize: 15 }}> {phone} </Text>
+                        </View>
+                    </Card>
+
+                    <Card style={styles.mycard} >
+                        <View style={styles.cardconent}>
+                            <FontAwesome5 style={{ margin: 4 }} name="home" size={24} color='#009999' />
+                            <Text style={{ marginTop: 12, fontSize: 15 }}> {password} </Text>
+                        </View>
+                    </Card>
+
+
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 15, marginBottom:50 }}>
+                        <Button icon="account-edit" color="#FF8C00" mode="contained" onPress={handleEdit}>
+                            Edit
+                        </Button>
+                    </View>
+                </View>
+            ) : (
+                <View>
+                    <Card style={styles.mycard}>
+                        <View style={styles.cardconent}>
+                            <Ionicons style={{ margin: 4 }} name="person" size={32} color='#009999' />
+                            <TextInput
+                                style={{ marginTop: 12, fontSize: 15 }}
+                                value={name}
+                                onChangeText={setName}
+                                autoFocus />
+                        </View>
+                    </Card>
+
+                    <Card style={styles.mycard} >
+                        <View style={styles.cardconent}>
+                            <MaterialIcons style={{ margin: 4 }} name="email" size={32} color='#009999' />
+                            <TextInput
+                                style={{ marginTop: 12, fontSize: 15 }}
+                                value={email}
+                                onChangeText={setEmail}
+                                autoFocus />
+
+                        </View>
+                    </Card>
+
+
+                    <Card style={styles.mycard} >
+                        <View style={styles.cardconent}>
+                            <Entypo style={{ margin: 4 }} name="phone" size={32} color='#009999' />
+                            <TextInput
+                                style={{ marginTop: 12, fontSize: 15 }}
+                                value={phone}
+                                onChangeText={setPhone}
+                                autoFocus />
+                        </View>
+                    </Card>
+
+                    <Card style={styles.mycard} >
+                        <View style={styles.cardconent}>
+                            <FontAwesome5 style={{ margin: 4 }} name="home" size={24} color='#009999' />
+                            <TextInput
+                                style={{ marginTop: 12, fontSize: 15 }}
+                                value={password}
+                                onChangeText={setPassword}
+                                autoFocus />
+                        </View>
+                    </Card>
+
+
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 15 , marginBottom:50 }}>
+                        <Button icon="account-edit" color="#FF8C00" mode="contained" onPress={handleSave}>
+                            Save
+                        </Button>
+                    </View>
+
+                </View>
+            )}
+        </ScrollView>
+    )
+}
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: 'white',
-     
-  },
-  imagestyle: {
-    alignItems: 'center',
-    marginTop: 30,
-    marginBottom:50
-  },
-  mycard: {
-    margin: 3,
-    marginTop: 10,
-  },
-  cardconent: {
-    flexDirection: 'row',
-    padding: 5,
-  },
-  divider: {
-    backgroundColor: '#009999',
-    height: 1,
-    flex: 1,
-    alignSelf: 'center',
-  },
-  input:{
-    borderWidth:2,
-    borderColor:'black',
-    width:200,
-    margin:5,
-    padding:10,
-    borderColor:'#009999'
-  }
-  
-});
-
+    root: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    imagestyle: {
+        alignItems: 'center',
+        marginTop: 30
+    },
+    mycard: {
+        margin: 3,
+        marginTop: 10
+    },
+    cardconent: {
+        flexDirection: 'row',
+        padding: 5
+    },
+    divider: {
+        backgroundColor: "#009999",
+        height: 1,
+        flex: 1,
+        alignSelf: 'center'
+    }
+})
 export default Profile;
